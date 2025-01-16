664ee1f90c15e4211b339438d60f09dfbc80b23f
B FAROOQ <bfarooq2022@gmail.com>
2025-01-16 11:46:31 +0530

    file

--- be/index.js
+++ be/index.js
@@ -13,7 +13,7 @@ app.use(cors(
     {
         origin: process.env.FRONTEND_URL,
         methods: 'GET, POST',
-        allowedHeaders: 'Content-Type, Authorization',
+        allowedHeaders: '*',
         credentials: true
     }
 ));
